b2d522f868b97973f3a4cd29e24ec1eeda006274
Chris Rowles <c.rowles@sentrybay.com>
2024-08-27 11:43:35 +0100

    more EDDN duplicate id64 exclusions

--- config/imports.php
+++ config/imports.php
@@ -1,13 +1,16 @@
 <?php
 
 return [
-  'errors' => [
-    'systems' => [
-      'exclusions' => [
-        'Haven',
-        "San Baltah’Sine",
-        "San Baltah'sine",
-      ]
+    'errors' => [
+        'systems' => [
+            'exclusions' => [
+                'Haven',             // integrity constraint, duplicate id64 22963579407080
+                "San Baltah’Sine",   // integrity constraint, duplicate id64 671491302793
+                "San Baltah'sine",   // integrity constraint, duplicate id64 671491302793
+                "New Chana",         // integrity constraint, duplicate id64 13865630901617
+                "Aventine",          // integrity constraint, duplicate id64 5069537879409
+                "Bedrock",           // integrity constraint, duplicate id64 5069806314881
+            ]
+        ]
     ]
-  ]
 ];
