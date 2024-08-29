f25f8192fd879167fa9c84a56dcc6dab34041710
Chris Rowles <c.rowles@sentrybay.com>
2024-08-29 05:28:56 +0100

    prepare check for existing FrontierUser

--- app/Services/Frontier/FrontierApiManager.php
+++ app/Services/Frontier/FrontierApiManager.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Api;
 
-use Exception;
-use Carbon\Carbon;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
-use App\Models\System;
 
 /**
  * Frontier auth client.
@@ -57,49 +53,6 @@ class FrontierApiManager
     }
 
     /**
-     * Fetch data from endpoints that require authentication.
-     *
-     * @param string $endpoint
-     * @param string $method
-     * @return mixed
-     */
-    public function fetch(
-        string $endpoint = '',
-        string $method = 'GET',
-        bool $isAssociated = false
-    ): mixed {
-        try {
-
-            $options = [];
-            if (session('character.access_token')) {
-                $options['headers'] = [
-                    'Authorization' => 'Bearer ' . session('character.access_token')
-                ];
-            }
-
-            if (! str_starts_with($endpoint, '/')) {
-                $endpoint = "/{$endpoint}";
-            }
-
-            $response = $this->client->request($method, $this->url . $endpoint, $options);
-        } catch (ClientException $e) {
-            $status = $e->getResponse()->getStatusCode();
-            if ($status === 403 || $status === 401) {
-                throw new Exception('Unauthorized, redirect to SSO login');
-            }
-
-            return false;
-        }
-
-        if ($response && $response->getStatusCode() === 200)
-        {
-            return json_decode($response->getBody()->getContents(), $isAssociated);
-        }
-
-        return false;
-    }
-
-    /**
      * Redirect to login to obtain an authorization token.
      *
      * return mixed
@@ -127,8 +80,10 @@ class FrontierApiManager
      */
     public function issueAccessToken(Request $request): mixed
     {
+        // Get the authorization code from the callback request
         $this->code = $request->get('code');
 
+        // Use it to obtain a valid access token
         $response = $this->client->request('POST', '/v2/oauth/token', [
             'auth' => [
                 $this->clientId,
@@ -140,54 +95,9 @@ class FrontierApiManager
             ]
         ]);
 
-        return json_decode($response->getBody()->getContents());
-    }
-
-    /**
-     * Refresh access token.
-     *
-     * @throws ClientException
-     * @return void
-     */
-    public function refreshAccessToken(): void
-    {
-        $response = $this->client->request('POST', '/v2/oauth/token', [
-            'auth' => [
-                $this->clientId,
-                $this->clientKey
-            ],
-            'form_params' => [
-                'grant_type' => 'refresh_token',
-                'refresh_token' => session('character.refresh_token')
-            ]
-        ]);
-
-        $auth = json_decode($response->getBody()->getContents());
-        $expires_on = Carbon::parse(Carbon::now())->addSeconds($auth->expires_in)->toIso8601String();
-
-        Session::put('character.access_token', $auth->access_token);
-        Session::put('character.expires_on', $expires_on);
-        Session::put('character.refresh_token', $auth->refresh_token);
-        Session::save();
-    }
-
-    /**
-     * Verify login and return character information.
-     *
-     * @return bool|mixed
-     * @throws ClientException
-     */
-    public function verifyAuthorization(): mixed
-    {
-        if (!session('character.access_token')) {
-            return false;
-        }
-
-        $response = $this->client->request('GET', '/oauth/verify', [
-            'headers' => [
-                'Authorization' => 'Bearer ' . session('character.access_token')
-            ]
-        ]);
+        // TODO check somewhere on FrontierUser, if the user is not in the database, create
+        //      the user record and the frontier user record.
+        
 
         return json_decode($response->getBody()->getContents());
     }
@@ -209,33 +119,5 @@ class FrontierApiManager
         }
 
         return $query;
-    }
-
-    /**
-     * Obtain information required for route.
-     *
-     * @param int $origin
-     * @param int $destination
-     * @return array
-     */
-    public function fetchRoute(int $origin, int $destination): array
-    {
-        $route = [];
-        $route['origin'] = System::whereSystemId($origin)->first();
-        $route['destination'] = System::whereSystemId($destination)->first();
-
-        $systems = $this->fetch('/route/'.$origin.'/'.$destination);
-
-        foreach ($systems as $id) {
-            $system = System::whereSystemId($id)
-                ->with('constellation.region')
-                ->first();
-                // dd($system);
-            $route['route'][] = $system;
-        }
-
-        $route['jumps'] = count($route['route']);
-
-        return $route;
     }
 }
