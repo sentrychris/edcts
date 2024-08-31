372d0eab7d34f327ef4f3a2140769dbcb01673b1
Chris Rowles <c.rowles@sentrybay.com>
2024-08-31 12:06:27 +0100

    Update CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        '*',
+        'http://localhost:4201',
+        'https://edcts.versyx.net'
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +33,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
