7d13eebb0914bd24db87e2d195eec309acc6138a
Chris Rowles <c.rowles@sentrybay.com>
2024-09-11 01:02:30 +0100

    Update eddn systems exclusions

--- config/imports.php
+++ config/imports.php
@@ -4,15 +4,19 @@ return [
     'errors' => [
         'systems' => [
             'exclusions' => [
-                'Haven',             // integrity constraint, duplicate id64 22963579407080
-                "San Baltah’Sine",   // integrity constraint, duplicate id64 671491302793
-                "San Baltah'sine",   // integrity constraint, duplicate id64 671491302793
-                "New Chana",         // integrity constraint, duplicate id64 13865630901617
-                "Aventine",          // integrity constraint, duplicate id64 5069537879409
-                "Bedrock",           // integrity constraint, duplicate id64 5069806314881
-                "HIP 19934",         // integrity constraint, duplicate id64 4206953927354
-                "Kara",              // integrity constraint, duplicate id64 4206953927354
-                "BD+56 477",         // integrity constraint, duplicate id64 4994892381
+                'Haven',              // integrity constraint, duplicate id64 22963579407080
+                "San Baltah’Sine",    // integrity constraint, duplicate id64 671491302793
+                "San Baltah'sine",    // integrity constraint, duplicate id64 671491302793
+                "New Chana",          // integrity constraint, duplicate id64 13865630901617
+                "Aventine",           // integrity constraint, duplicate id64 5069537879409
+                "Bedrock",            // integrity constraint, duplicate id64 5069806314881
+                "HIP 19934",          // integrity constraint, duplicate id64 4206953927354
+                "Kara",               // integrity constraint, duplicate id64 4206953927354
+                "BD+56 477",          // integrity constraint, duplicate id64 4994892381
+                "NGC 2168 BSB 5180",  // integrity constraint, duplicate name 22665750847745
+                "NGC 2168 BSB 5175",  // integrity constraint, duplicate name 27063797293313
+                "NGC 2168 SB 254",    // integrity constraint, duplicate name 20465922154929
+                "GMB2010 WOCS 24056", // integrity constraint, duplicate name 7272587993345
             ]
         ]
     ]
