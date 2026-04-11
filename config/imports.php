4299004fd31427dcbea532cbf811efaca31ca32c
christopher rowles <c.rowles@sentrybay.com>
2026-04-11 11:48:42 +0100

    exclusion

--- config/imports.php
+++ config/imports.php
@@ -4,23 +4,23 @@ return [
     'errors' => [
         'systems' => [
             'exclusions' => [
-                'Haven',              // integrity constraint, duplicate id64 22963579407080
-                "San Baltah’Sine",    // integrity constraint, duplicate id64 671491302793
-                "San Baltah'sine",    // integrity constraint, duplicate id64 671491302793
-                "New Chana",          // integrity constraint, duplicate id64 13865630901617
-                "Aventine",           // integrity constraint, duplicate id64 5069537879409
-                "Bedrock",            // integrity constraint, duplicate id64 5069806314881
-                "HIP 19934",          // integrity constraint, duplicate id64 4206953927354
-                "Kara",               // integrity constraint, duplicate id64 4206953927354
-                "BD+56 477",          // integrity constraint, duplicate id64 4994892381
-                "NGC 2168 BSB 5180",  // integrity constraint, duplicate name 22665750847745
-                "NGC 2168 BSB 5175",  // integrity constraint, duplicate name 27063797293313
-                "NGC 2168 SB 254",    // integrity constraint, duplicate name 20465922154929
-                "GMB2010 WOCS 24056", // integrity constraint, duplicate name 7272587993345
-                "2MASS J05362745+0945256", // integrity constraint, duplicate name 84238937432937
-                "NGC 2168 SB 791",
-                "NGC 2168 BSB 5280"
-            ]
+                'Haven',                   // integrity constraint, duplicate, id64 22963579407080
+                "San Baltah’Sine",         // integrity constraint, duplicate, id64 671491302793
+                "San Baltah'sine",         // integrity constraint, duplicate, id64 671491302793
+                "New Chana",               // integrity constraint, duplicate, id64 13865630901617
+                "Aventine",                // integrity constraint, duplicate, id64 5069537879409
+                "Bedrock",                 // integrity constraint, duplicate, id64 5069806314881
+                "HIP 19934",               // integrity constraint, duplicate, id64 4206953927354
+                "Kara",                    // integrity constraint, duplicate, id64 4206953927354
+                "BD+56 477",               // integrity constraint, duplicate, id64 4994892381
+                "NGC 2168 BSB 5180",       // integrity constraint, duplicate, id64 22665750847745
+                "NGC 2168 BSB 5175",       // integrity constraint, duplicate, id64 27063797293313
+                "NGC 2168 SB 254",         // integrity constraint, duplicate, id64 20465922154929
+                "GMB2010 WOCS 24056",      // integrity constraint, duplicate, id64 7272587993345
+                "2MASS J05362745+0945256", // integrity constraint, duplicate, id64 84238937432937
+                "NGC 2168 SB 791",         // integrity constraint, duplicate, id64 57849854435657
+                "NGC 2168 BSB 5280"        // integrity constraint, duplicate, id64 5073296302449
+            ],
         ]
     ]
 ];
